aa0d50f9d53cf1ef9c2dbbb7b98e87668396b54c
Arpitkumar Soni <soniarpi@sheridancollege.ca>
2021-01-14 19:35:23 -0500

    Final Commit

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-const app =express();
+const app = express();
 
-app.get('/', (req, res) => res.json({msg: "Welcome to the ContactKeeper API..."}));
+// Connect Database
+connectDB();
 
-//define routes...
-app.use('/api/users',require('./routes/users.js'));  
-app.use('/api/auth',require('./routes/auth.js'));  
-app.use('/api/contacts',require('./routes/contacts.js'));  
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;  //will look for any environment var first otherwise port num 5000
-    
-app.listen(PORT, () => console.log(`Server stared on port ${PORT}`));  
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
